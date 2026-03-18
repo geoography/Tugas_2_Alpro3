1d272b0c22bcd555e0a4c40cddd81835205a6b92
Geofrey Melvern <melverng23@gmail.com>
2026-03-18 15:50:23 +0700

    Implement scholarship eligibility program
    The program determines scholarship categories based on student GPA and parental income. It includes input prompts and conditional logic to classify the scholarship eligibility.

--- soalTugas3.cpp
+++ soalTugas3.cpp
@@ -1,23 +1,30 @@
-/*
-Lembaga Kemahasiswaan USU memiliki sebuah program yang menentukan kategori beasiswa yang dapat diterima oleh mahasiswa yang mengajukan beasiswa. Penentuan beasiswa ini didasarkan pada Indeks Prestasi (IP) mahasiswa dan Gaji Orang Tua.
+#include <iostream>
+using namespace std;
 
-Ketentuan Penerimaan Beasiswa
-1. Mahasiswa dengan IP ≥ 3.5 otomatis berhak atas beasiswa kategori 4.
-2.  Mahasiswa berhak atas beasiswa kategori 1 jika:
-- Gaji orang tua kurang dari 1 juta rupiah.
-- IP mahasiswa kurang dari 3.5.
-3. Mahasiswa berhak atas beasiswa kategori 2 atau 3 jika:
-- Gaji orang tua berada di rentang 1 juta - 5 juta rupiah.
-- IP mahasiswa kurang dari 3.5:
-    - Jika IP ≥ 2.0, berhak atas beasiswa kategori 3.
-    - Jika IP < 2.0, berhak atas beasiswa kategori 2.
-4. Mahasiswa yang tidak memenuhi kriteria di atas tidak mendapatkan beasiswa.
+int main() {
+    float ip;
+    long long gaji;
+    cout << "Input IP: "; // input IP 
+    cin >> ip;
+    cout << "Input gaji orang tua: Rp"; // input gaji orang tua
+    cin >> gaji;
 
-Buatlah program yang membaca 2 input berupa bilangan riil (float):
-IP mahasiswa (contoh: 3.51)
-Gaji orang tua dalam juta rupiah (contoh: 1.5, yang berarti 1.5 juta rupiah)
-Program harus mencetak kategori beasiswa yang diperoleh mahasiswa sesuai dengan ketentuan di atas. Jika mahasiswa tidak berhak atas beasiswa, cetak “Tidak berhak mendapatkan beasiswa”.
+    if (ip >= 3.5) { // jika IP >= 3.5 sudah pasti kategori 4
+        cout << "Kategori 4";
+    } 
+    else if (gaji < 1000000 && ip < 3.5) { // jika gaji orang tua lebih kecil dari 1jt dan ip di bawah 3.5 berhak mendapat beasiswa kategori 1
+        cout << "Kategori 1";
+    } 
+    else if (gaji >= 1000000 && gaji <= 5000000 && ip < 3.5) { // rentang gaji orang tua dari 1jt sampai 5jt dan ip di bawah 3.5 berhak mendapat beasiswa kategori 2 atau 3
+        if (ip >= 2.0) { 
+            cout << "Kategori 3"; // ip lebih besar sama dengan 2 berhak mendapat beasiswa kategori 3
+        } else {
+            cout << "Kategori 2"; // ip di bawah 2 berhak mendapat beasiswa kategori 2
+        }
+    } 
+    else { // selain dari semua syarat di atas tidak berhak mendapat beasiswa
+        cout << "Tidak berhak mendapatkan beasiswa";
+    }
 
-*/
-
-// Hapus komentar yang berisi soal sebelum memasukkan kode untuk jawaban
+    return 0;
+}
